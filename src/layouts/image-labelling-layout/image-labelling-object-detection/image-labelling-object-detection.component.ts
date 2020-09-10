fd33bd0490498d5cdc05c41584c85806fb4164ef
BobbyLCW <mfbobbylee@gmail.com>
2020-09-10 18:19:57 +0800

    Creation of Bobby Branch

---
+++ src/layouts/image-labelling-layout/image-labelling-object-detection/image-labelling-object-detection.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-labelling-object-detection',
+  templateUrl: './image-labelling-object-detection.component.html',
+  styleUrls: ['./image-labelling-object-detection.component.css'],
+})
+export class ImageLabellingObjectDetectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
