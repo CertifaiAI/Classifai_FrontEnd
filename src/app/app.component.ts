f5d5b1060a5f5a714eaa8decce28c3931ece52a1
Daniel Jie <daniel.lim@nexent.co>
2020-10-13 16:43:53 +0800

    Code Amends
    Removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ThemeService } from 'src/shared/services/theme.service';
 
 @Component({
