c9cfbf50c73bca00576c6ac3a8ab75f8d96d4db5
BobbyLCW <mfbobbylee@gmail.com>
2020-12-22 16:17:23 +0800

    added empty image seg component

---
+++ src/layouts/image-labelling-layout/image-labelling-segmentation/image-labelling-segmentation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-image-labelling-segmentation',
+    templateUrl: './image-labelling-segmentation.component.html',
+    styleUrls: ['./image-labelling-segmentation.component.scss'],
+})
+export class ImageLabellingSegmentationComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
