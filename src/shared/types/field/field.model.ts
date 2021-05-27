89c613b2a26a6621bc3d3aa9aa0babb037f30e13
Han Sheng-Certifai <hansheng.liang@skymind.my>
2021-05-27 15:54:47 +0800

    :page_facing_up: Add Licensing Header

--- src/shared/types/field/field.model.ts
+++ src/shared/types/field/field.model.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Use of this source code is governed by Apache License 2.0 that can be
+ * found in the LICENSE file at https://github.com/CertifaiAI/Classifai_FrontEnd/blob/main/LICENSE
+ */
+
 /** @type responsible for getting type system for event.target.value */
 export type HTMLElementEvent<T extends HTMLElement> = Event & {
     target: T;
--- src/shared/types/with-optional/with-optional.ts
+++ src/shared/types/with-optional/with-optional.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Use of this source code is governed by Apache License 2.0 that can be
+ * found in the LICENSE file at https://github.com/CertifaiAI/Classifai_FrontEnd/blob/main/LICENSE
+ */
+
 type AllKeyOf<T> = T extends never ? never : keyof T;
 
 type Omit<T, K> = { [P in Exclude<keyof T, K>]: T[P] };
