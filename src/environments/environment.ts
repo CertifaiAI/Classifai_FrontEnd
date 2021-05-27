ecc74039bc2b6a41d31b7e2eadca48b345f8cc2d
Daniel Jie <daniel.lim@nexent.co>
2021-05-27 12:30:23 +0800

    🗑️ Removal of aws config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,6 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
     production: false,
     baseURL: 'http://localhost:4200/',
-    awsBucketName: `classifai-v2`,
-    awsAccessKeyId: '44LLO49S6VBXQ6APPW56',
-    awsSecretAccessKey: 'Diy1eCM9tNlzwqlwNS1eANSyucu2mePwJkdLXae0',
 };
 
 /*
