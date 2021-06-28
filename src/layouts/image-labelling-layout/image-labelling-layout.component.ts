4dc7750551edcd32f70531034510e3080a4103fd
Han Sheng-Certifai <hansheng.liang@skymind.my>
2021-06-28 12:55:00 +0800

    :bug: Change Extraction Method based on OS

--- src/layouts/image-labelling-layout/image-labelling-layout.component.ts
+++ src/layouts/image-labelling-layout/image-labelling-layout.component.ts
@@ -876,10 +876,20 @@ export class ImageLabellingLayoutComponent implements OnInit, OnDestroy {
     }
 
     onRenameImage(thumbnailInfo: CompleteMetadata) {
-        this.imgPathSplit = thumbnailInfo.img_path.split('\\');
-        const imageName = this.imgPathSplit.pop();
-        this.newImageName = imageName ? imageName.split('.')[0] : '';
-        this.imageExt = imageName ? '.' + imageName.split('.').pop() : '';
+        const platform = window.navigator.platform;
+
+        if (platform.startsWith('Mac') || platform.startsWith('Linux')) {
+            this.imgPathSplit = thumbnailInfo.img_path.split('/');
+            const imageName = this.imgPathSplit.pop();
+            this.newImageName = imageName ? imageName.split('.')[0] : '';
+            this.imageExt = imageName ? '.' + imageName.split('.').pop() : '';
+        } else {
+            this.imgPathSplit = thumbnailInfo.img_path.split('/');
+            const imageName = this.imgPathSplit.pop();
+            this.newImageName = imageName ? imageName.split('.')[0] : '';
+            this.imageExt = imageName ? '.' + imageName.split('.').pop() : '';
+        }
+
         this.selectedUuid = thumbnailInfo.uuid;
         this.renameImageErrorCode = 0;
         this._modalService.open(this.modalRenameImage);
@@ -900,8 +910,16 @@ export class ImageLabellingLayoutComponent implements OnInit, OnDestroy {
             .subscribe((res) => {
                 if (res.message === 1) {
                     const index = this.thumbnailList.findIndex((t) => t.uuid === this.selectedUuid);
-                    this.thumbnailList[index].img_path =
-                        this.imgPathSplit.join('\\') + '\\' + this.newImageName + this.imageExt;
+
+                    const platform = window.navigator.platform;
+                    if (platform.startsWith('Mac') || platform.startsWith('Linux')) {
+                        this.thumbnailList[index].img_path =
+                            this.imgPathSplit.join('/') + '/' + this.newImageName + this.imageExt;
+                    } else {
+                        this.thumbnailList[index].img_path =
+                            this.imgPathSplit.join('\\') + '\\' + this.newImageName + this.imageExt;
+                    }
+
                     this.newImageName = '';
                     this._modalService.close(this.modalRenameImage);
                 } else {
--- src/shared/pipe/file-name.pipe.ts
+++ src/shared/pipe/file-name.pipe.ts
@@ -16,7 +16,7 @@ export class FileNamePipe implements PipeTransform {
     transform(name: string): string {
         const newName = name
             ? // if browser detected OS that starts with Mac just trim in OS's path
-              window.navigator.platform.startsWith('Mac')
+              window.navigator.platform.startsWith('Mac') || window.navigator.platform.startsWith('Linux')
                 ? name.split('/').slice(-1)[0]
                 : name.split('\\').slice(-1)[0]
             : '';
