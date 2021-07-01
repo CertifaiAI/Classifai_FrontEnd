4af28122b0c0878b5d5c89474d8f4d80ee46c6c0
amalsyahmi-certifai <76939137+amalsyahmi-certifai@users.noreply.github.com>
2021-07-01 15:55:26 +0800

    :recycle: Fix code smell

--- src/shared/services/shortcut-key.service.ts
+++ src/shared/services/shortcut-key.service.ts
@@ -4,8 +4,6 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class ShortcutKeyService {
-    constructor() {}
-
     checkKey(ctrlKey: boolean, metaKey: boolean, shiftKey: boolean, key: string, keyCheck: string) {
         let isCorrectKey = false;
         const modKey = ctrlKey || metaKey;
