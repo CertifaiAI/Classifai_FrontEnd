8937b3e837bf542f976cd7def2dd522a2febc8e0
amalsyahmi-certifai <76939137+amalsyahmi-certifai@users.noreply.github.com>
2021-07-06 18:14:44 +0800

    :recycle: Refactor code smell in server error component

--- src/components/server-error/server-error.component.ts
+++ src/components/server-error/server-error.component.ts
@@ -4,16 +4,13 @@
  * found in the LICENSE file at https://github.com/CertifaiAI/Classifai_FrontEnd/blob/main/LICENSE
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'server-error',
     templateUrl: './server-error.component.html',
     styleUrls: ['./server-error.component.scss'],
 })
-export class ServerErrorComponent implements OnInit {
+export class ServerErrorComponent {
     public errorMessage: string = 'Server Probably is Down, Please Contact Administrator!';
-    constructor() {}
-
-    ngOnInit(): void {}
 }
