c3dfc1b63d7bc374ed5e25446ab4ab1db42e2dfe
Han Sheng-Certifai <hansheng.liang@skymind.my>
2021-07-27 09:59:53 +0800

    :construction: Add Frame Extraction Service

--- src/components/video-labelling/video-frame-extraction.service.ts
+++ src/components/video-labelling/video-frame-extraction.service.ts
@@ -1,9 +1,3 @@
-/**
- * @license
- * Use of this source code is governed by Apache License 2.0 that can be
- * found in the LICENSE file at https://github.com/CertifaiAI/Classifai_FrontEnd/blob/main/LICENSE
- */
-
 import { Injectable } from '@angular/core';
 
 enum VideoToFramesMethod {
@@ -14,27 +8,30 @@ enum VideoToFramesMethod {
 @Injectable({
     providedIn: 'root',
 })
-export class VideoFrameExtractionService {
+export class FrameExtractionService {
     constructor() {}
 
     /**
      * Extracts frames from the video and returns them as an array of imageData
      * @param videoUrl url to the video file (html5 compatible format) eg: mp4
      * @param amount number of frames per second or total number of frames that you want to extract
-     * @param type [fps, totalFrames] The method of extracting frames: Number of frames per second of video or
-     * the total number of frames acros the whole video duration. defaults to fps
+     * @param type [fps, totalFrames] The method of extracting frames: Number of frames per second of video or the total number of frames acros the whole video duration. defaults to fps
      */
-    public static getFrames(videoUrl: string, amount: number, type: VideoToFramesMethod): Promise<ImageData[]> {
+    getFrames(
+        videoUrl: string,
+        amount: number,
+        type: VideoToFramesMethod = VideoToFramesMethod.fps,
+    ): Promise<ImageData[]> {
         return new Promise((resolve: (frames: ImageData[]) => void, reject: (error: string) => void) => {
-            const frames: ImageData[] = [];
-            const canvas: HTMLCanvasElement = document.createElement('canvas');
-            const context: CanvasRenderingContext2D = (canvas.getContext('2d') as unknown) as CanvasRenderingContext2D;
+            let frames: ImageData[] = [];
+            let canvas: HTMLCanvasElement = document.createElement('canvas');
+            let context: CanvasRenderingContext2D = (canvas.getContext('2d') as unknown) as CanvasRenderingContext2D;
             let duration: number;
 
-            const video = document.createElement('video');
+            let video = document.createElement('video');
             video.preload = 'auto';
-            const that = this;
-            video.addEventListener('loadeddata', async () => {
+            let that = this;
+            video.addEventListener('loadeddata', async function () {
                 canvas.width = video.videoWidth;
                 canvas.height = video.videoHeight;
                 duration = video.duration;
@@ -53,13 +50,9 @@ export class VideoFrameExtractionService {
         });
     }
 
-    private static getVideoFrame(
-        video: HTMLVideoElement,
-        context: CanvasRenderingContext2D,
-        time: number,
-    ): Promise<ImageData> {
+    getVideoFrame(video: HTMLVideoElement, context: CanvasRenderingContext2D, time: number): Promise<ImageData> {
         return new Promise((resolve: (frame: ImageData) => void, reject: (error: string) => void) => {
-            const eventCallback = () => {
+            let eventCallback = () => {
                 video.removeEventListener('seeked', eventCallback);
                 this.storeFrame(video, context, resolve);
             };
@@ -68,11 +61,7 @@ export class VideoFrameExtractionService {
         });
     }
 
-    private static storeFrame(
-        video: HTMLVideoElement,
-        context: CanvasRenderingContext2D,
-        resolve: (frame: ImageData) => void,
-    ) {
+    storeFrame(video: HTMLVideoElement, context: CanvasRenderingContext2D, resolve: (frame: ImageData) => void) {
         context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
         resolve(context.getImageData(0, 0, video.videoWidth, video.videoHeight));
     }
